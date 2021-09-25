bf437c28242adb1318f0a04253620c828afdc133
Tom Leung <hoholee8992@gmail.com>
2021-09-25 18:46:43 +0800

    test

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -12,11 +12,19 @@ def evaluate_decoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     values, slots, history = [data["possible_values"], data["num_slots"], data["history"]]
-    return json.dumps({"answer" : random_guess(slots, values, history)})
+    return json.dumps({"answer" : backward_guess(slots, values, history)})
 
 def random_guess(slots, values, history): # check right symbol in wrong / right position
     answer_list = []
     for x in range(slots):
+        answer_list.append(values[x])
+    logging.info("value:", values, "slots", slots, "history", history)
+    logging.info("answer_list", answer_list)
+    return answer_list
+
+def backward_guess(slots, values, history):
+    answer_list = []
+    for x in reversed(range(slots)):
         answer_list.append(values[x])
     logging.info("value:", values, "slots", slots, "history", history)
     logging.info("answer_list", answer_list)
