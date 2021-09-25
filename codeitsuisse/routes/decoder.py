c1f581bb7f043ed271e6985e2e00e93ddfac63f3
Tom Leung <hoholee8992@gmail.com>
2021-09-25 19:17:50 +0800

    .'

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -14,7 +14,7 @@ def evaluate_decoder():
     values, slots, history = [data["possible_values"], data["num_slots"], data["history"]]
     # return json.dumps({"answer" : backward_guess(slots, values, history)})
     # return json.dumps({"answer": backward_guess(slots, values, history)})
-    return json.dumps({"answer": ["e, o, h, l p"]})
+    return json.dumps({"answer": ["e", "o", "h", "l", "p"]})
 
 def forward_guess(slots, values, history): # check right symbol in wrong / right position
     answer_list = []
