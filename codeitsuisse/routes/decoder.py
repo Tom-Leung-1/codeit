3859df114f1c8f9d60cef6b0f26bbb351db808a2
Tom Leung <hoholee8992@gmail.com>
2021-09-25 18:34:49 +0800

    decode

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -12,7 +12,11 @@ def evaluate_decoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     values, slots, history = [data["possible_values"], data["num_slots"], data["history"]]
+    answer_list = []
+    for x in range(slots):
+        answer_list.append(values[0])
     logging.info("value:", values, "slots", slots, "history", history)
-    return json.dumps({})
+    logging.info("answer_list", answer_list)
+    return json.dumps({"answer" : answer_list})
 
 
