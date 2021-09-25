994a418b345e379cd085c0a1e1ed24a70d086bd3
Tom Leung <hoholee8992@gmail.com>
2021-09-25 22:21:08 +0800

    add oil

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -14,30 +14,35 @@ def evaluate_decoder():
     values, slots, history = [data["possible_values"], data["num_slots"], data["history"]]
     if len(history) == 0: return json.dumps({"answer" : forward_guess(slots, values, history)})
     if len(history) == 1: return json.dumps({"answer": next_guess(slots, values, history, 0, 5)})
-    if len(history) == 2: return json.dumps({"answer": next_guess(slots, values, history, 0, 6)})
-    #ans, count_dict = study_2(history)
+    if len(history) == 2:
+        ans, counts, excludes = study_2(history, 0, 1)
+        logging.info("ans", ans, counts, excludes)
+        return json.dumps({"answer": next_guess(slots, values, history, 0, 6)})
     if len(history) == 3: return json.dumps({"answer": next_guess(slots, values, history, 1, 2)})
     if len(history) == 4: return json.dumps({"answer": next_guess(slots, values, history, 2, 3)})
     if len(history) == 5: return json.dumps({"answer": next_guess(slots, values, history, 3, 4)})
     if len(history) == 6: return json.dumps({"answer": next_guess(slots, values, history, 4, 5)})
     return json.dumps({"answer": ["w", "w", "w", "l", "l"]})
 
-def study_2(history, values):
+def study_2(h, _0, _1):
+    history = reversed(h)
     ans = [0, 0, 0, 0, 0]
-    count_dict = {}
-    for v in values:
-        count_dict[v] = 0
-    rw0, rr0 = list(str(history[0]["result"])) if history[0]["result"] >= 10 else [0, history[0]["result"]]
-    rw1, rr1 = list(str(history[1]["result"])) if history[0]["result"] >= 10 else [0, history[0]["result"]]
+    counts = []
+    excludes = []
+    rw0, rr0 = list(str(history[_0]["result"])) if history[_0]["result"] >= 10 else [0, history[_0]["result"]]
+    rw1, rr1 = list(str(history[_1]["result"])) if history[_0]["result"] >= 10 else [0, history[_0]["result"]]
     rw0, rr0, rw1, rr1 = [int(rw0), int(rr0), int(rw1), int(rr1)]
     if (rw0 < rw1): # old leave new come
-        count_dict[history[1]["output_received"][0]] += 1
+        counts.append(history[_1]["output_received"][0])
+        excludes.append(history[_0]["output_received"][0])
     elif (rw0 > rw1):
-        count_dict[history[0]["output_received"][0]] += 1
+        counts.append(history[_0]["output_received"][0])
+        excludes.append(history[_1]["output_received"][0])
     if (rr0 < rr1): # new is correct
-        ans[0] = history[1]["output_received"][0]
+        ans[0] = history[_1]["output_received"][0]
     elif (rr0 > rr1):
-        ans[0] = history[0]["output_received"][0]
+        ans[0] = history[_0]["output_received"][0]
+    return ans, counts, excludes
 
 
 
