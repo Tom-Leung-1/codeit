75f1302454cde5083c4f29a672ef65c80baca6fc
Tom Leung <hoholee8992@gmail.com>
2021-09-25 15:26:41 +0800

    p3

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -20,22 +20,27 @@ def evaluate_parasite():
 
 def parasite(test_case):
     queue = deque()
+    queue_b = deque()
     room, grid, interested_ind = [test_case['room'], test_case["grid"], test_case["interestedIndividuals"]]
     infect = [[ 0 if x == 3 else -1 for x in row] for row in grid]
+    infect_b = [[ 0 if x == 3 else -1 for x in row] for row in grid]
     # print("infect_initial", infect)
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 3:
                 queue.append([row, col, 0])
+                queue_b.append([row, col, 0])
                 # infection(infect, grid, row, col, 0)
     bfs_infection(queue, grid, infect)
+    bfs_infection_B(queue_b, grid, infect_b)
     p1_dict = {}
     # print("infect_final", infect)
     # print("grid", grid)
     for x in interested_ind:
         p1_dict[x] = get_ind(x, infect)
     p2 = get_time_A(infect, grid)
-    return {"room": room, "p1" : p1_dict, "p2": p2, "p3": 0, "p4": 0}
+    p3 = get_time_A(infect_b, grid)
+    return {"room": room, "p1" : p1_dict, "p2": p2, "p3": p3, "p4": 0}
 
 
 def get_ind(x, infect):
@@ -72,6 +77,42 @@ def bfs_infection(queue, grid, infect):
             # print("from", row, col, "to", row, col + 1, "time", time + 1)
             queue.append([row, col + 1, time + 1])
             infect[row][col + 1] = time + 1
+
+def bfs_infection_B(queue, grid, infect):
+    while len(queue):
+        row, col, time = queue.popleft()
+        if row - 1 >= 0 and (infect[row - 1][col] == -1 or time + 1 < infect[row - 1][col]) and grid[row - 1][col] == 1:
+            # print("from", row, col, "to", row-1, col, "time", time+1)
+            queue.append([row-1, col, time + 1])
+            infect[row-1][col] = time + 1
+        if row + 1 < len(grid) and (infect[row + 1][col] == -1 or time + 1 < infect[row + 1][col]) and grid[row + 1][col] == 1:
+            # print("from", row, col, "to", row + 1, col, "time", time + 1)
+            queue.append([row + 1, col, time + 1])
+            infect[row + 1][col] = time + 1
+        if col - 1 >= 0 and (infect[row][col - 1] == -1 or time + 1 < infect[row][col - 1]) and grid[row][col - 1] == 1:
+            # print("from", row, col, "to", row, col - 1, "time", time + 1)
+            queue.append([row, col - 1, time + 1])
+            infect[row][col - 1] = time + 1
+        if col + 1 < len(grid[0]) and (infect[row][col + 1] == -1 or time + 1 < infect[row][col + 1]) and grid[row][col + 1] == 1:
+            # print("from", row, col, "to", row, col + 1, "time", time + 1)
+            queue.append([row, col + 1, time + 1])
+            infect[row][col + 1] = time + 1
+        if row - 1 >= 0 and col - 1 >= 0 and (infect[row - 1][col-1] == -1 or time + 1 < infect[row - 1][col-1]) and grid[row - 1][col-1] == 1:
+            # print("from", row, col, "to", row-1, col, "time", time+1)
+            queue.append([row-1, col-1, time + 1])
+            infect[row-1][col-1] = time + 1
+        if row - 1 >= 0 and col + 1 < len(grid[0]) and (infect[row - 1][col+1] == -1 or time + 1 < infect[row - 1][col+1]) and grid[row - 1][col+1] == 1:
+            # print("from", row, col, "to", row-1, col, "time", time+1)
+            queue.append([row-1, col+1, time + 1])
+            infect[row-1][col+1] = time + 1
+        if row + 1 < len(grid) and col - 1 >= 0 and (infect[row + 1][col-1] == -1 or time + 1 < infect[row + 1][col-1]) and grid[row + 1][col-1] == 1:
+            # print("from", row, col, "to", row-1, col, "time", time+1)
+            queue.append([row+1, col-1, time + 1])
+            infect[row+1][col-1] = time + 1
+        if row + 1 < len(grid) and col + 1 < len(grid[0]) and (infect[row + 1][col+1] == -1 or time + 1 < infect[row + 1][col+1]) and grid[row + 1][col+1] == 1:
+            # print("from", row, col, "to", row-1, col, "time", time+1)
+            queue.append([row+1, col+1, time + 1])
+            infect[row+1][col+1] = time + 1
 
 def infection(infect, grid, row, col, time):
     # print("infect", row, col, time)
