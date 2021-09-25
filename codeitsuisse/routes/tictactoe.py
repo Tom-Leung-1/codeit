db90cfacfdebd107aea84802f3abf9ef9b53f013
Tom Leung <hoholee8992@gmail.com>
2021-09-25 16:58:30 +0800

    tst

--- codeitsuisse/routes/tictactoe.py
+++ codeitsuisse/routes/tictactoe.py
@@ -6,19 +6,20 @@ from flask import request, jsonify
 from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
-import requests
+# import requests
 
 @app.route('/tic-tac-toe', methods=['POST'])
 def evaluate():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    id = data.get("batteId")
-    url = "https://cis2021-arena.herokuapp.com/tic-tac-toe/play/" + id
-    print(url)
-    response = requests.get(url)
-    x = requests.post(url, data={})
-    logging.info(response)
-    logging.info(x)
-    return json.dumps(response)
+    pass
+    # data = request.get_json()
+    # logging.info("data sent for evaluation {}".format(data))
+    # id = data.get("batteId")
+    # url = "https://cis2021-arena.herokuapp.com/tic-tac-toe/play/" + id
+    # print(url)
+    # response = requests.get(url)
+    # x = requests.post(url, data={})
+    # logging.info(response)
+    # logging.info(x)
+    # return json.dumps(response)
 
 
