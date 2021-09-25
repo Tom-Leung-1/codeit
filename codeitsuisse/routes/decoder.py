861b003e904dc6f3ef51bf4f26e4387a7b56a55e
Tom Leung <hoholee8992@gmail.com>
2021-09-25 22:52:10 +0800

    test

--- codeitsuisse/routes/decoder.py
+++ codeitsuisse/routes/decoder.py
@@ -12,23 +12,47 @@ def evaluate_decoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
     values, slots, history = [data["possible_values"], data["num_slots"], data["history"]]
-    if len(history) == 0: return json.dumps({"answer" : forward_guess(slots, values, history)})
-    if len(history) == 1: return json.dumps({"answer": next_guess(slots, values, history, 0, 5)})
-    if len(history) == 2:
-        ans, counts, excludes = study_2(history, 0, 1)
-        logging.info("ans", ans, counts, excludes)
-        return json.dumps({"answer": next_guess(slots, values, history, 0, 6)})
-    if len(history) == 3: return json.dumps({"answer": next_guess(slots, values, history, 1, 2)})
-    if len(history) == 4: return json.dumps({"answer": next_guess(slots, values, history, 2, 3)})
-    if len(history) == 5: return json.dumps({"answer": next_guess(slots, values, history, 3, 4)})
-    if len(history) == 6: return json.dumps({"answer": next_guess(slots, values, history, 4, 5)})
-    return json.dumps({"answer": ["h", "a", "t", "g", "g"]})
-
-def study_2(h, _0, _1):
-    history = h[::-1]
     ans = [0, 0, 0, 0, 0]
     counts = []
     excludes = []
+    if len(history) == 0: return json.dumps({"answer" : forward_guess(slots, values, history)})
+    if len(history) == 1: return json.dumps({"answer": next_guess(slots, values, history, 0, 5)})
+    if len(history) >= 2:
+        for x in range (len(history)):
+            ans, counts, excludes = study_2(history, x, x+1, ans, counts, excludes)
+        old, new = analysis(ans, values, excludes)
+        logging.info("ans", ans, counts, excludes)
+        return json.dumps({"answer": next_guess(slots, values, history, old, new)})
+    # if len(history) == 3:
+    #     ans, counts, excludes = study_2(history, 0, 1)
+    #     ans, counts, excludes = study_2(history, 1, 2, ans, counts, excludes)
+    #     old, new = analysis(ans, values, excludes)
+    #     return json.dumps({"answer": next_guess(slots, values, history, old, new)})
+    # if len(history) == 4:
+    #     ans, counts, excludes = study_2(history, 0, 1)
+    #     ans, counts, excludes = study_2(history, 1, 2, ans, counts, excludes)
+    #     old, new = analysis(ans, values, excludes)
+    #     return json.dumps({"answer": next_guess(slots, values, history, 2, 3)})
+    # if len(history) == 5: return json.dumps({"answer": next_guess(slots, values, history, 3, 4)})
+    # if len(history) == 6: return json.dumps({"answer": next_guess(slots, values, history, 4, 5)})
+    return json.dumps({"answer": ["h", "a", "t", "g", "g"]})
+
+def analysis(ans, values, excludes, ):
+    old = None
+    for x in ans:
+        if x == 0:
+            old = x
+            break
+    new_list = list(set(values) - set(excludes))
+    find = new_list[0]
+    for idx, v in enumerate(values):
+        if find == v:
+            return old, idx
+
+
+
+def study_2(h, _0, _1, ans=[0, 0, 0, 0, 0], counts = [], excludes = []):
+    history = h[::-1]
     rw0, rr0 = list(str(history[_0]["result"])) if history[_0]["result"] >= 10 else [0, history[_0]["result"]]
     rw1, rr1 = list(str(history[_1]["result"])) if history[_0]["result"] >= 10 else [0, history[_0]["result"]]
     rw0, rr0, rw1, rr1 = [int(rw0), int(rr0), int(rw1), int(rr1)]
@@ -54,10 +78,13 @@ def forward_guess(slots, values, history): # check right symbol in wrong / right
     logging.info("forward_guess:", "value:", values, "slots", slots, "history", history, "answer_list", answer_list)
     return answer_list
 
-def next_guess(slots, values, history, old, new):
+def next_guess(slots, values, history, old, new, ans):
     answer_list = []
     for x in range(slots):
         answer_list.append(values[x])
+    for idx, a in enumerate(ans):
+        if a != 0:
+            answer_list[idx] = a
     answer_list[old] = values[new]
     logging.info("forward_guess:", "value:", values, "slots", slots, "history", history, "answer_list", answer_list)
     return answer_list
